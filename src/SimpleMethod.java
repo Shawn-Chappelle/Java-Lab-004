61bed16e327ac3843254eb527b251fb222fade17
Shawn Chappelle <shachappelle17@gmail.com>
2022-09-14 11:55:45 -0700

    Made the changes as described in the assignment, seems to work to me.

--- src/SimpleMethod.java
+++ src/SimpleMethod.java
@@ -1,5 +1,10 @@
 public class SimpleMethod {
     // Put your method code here
+    public static void square(int base) {
+        System.out.println(base * base);
+
+
+    }
 
     public static void main(String[] args) {
         // This is the method call, create the square method.
--- src/SimpleMethod2.java
+++ src/SimpleMethod2.java
@@ -2,11 +2,19 @@ import java.util.Scanner;
 
 public class SimpleMethod2 {
     // Put your method code here, takes 1 integer, squares it, returns the integer result of the square.
+    public static int square(int input) {
+        int result =(input * input);
 
+
+        return result;
+    }
     public static void main(String[] args) {
-        // Put scanner code to get integer input here
+        System.out.println("Insert Integer to be Squared:");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
         // Modify the below call to assign the result of the method call to a variable.
         square(input);
         // Put the result System.out code here.
+        System.out.println(square(input));
     }
 }
